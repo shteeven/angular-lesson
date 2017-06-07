d96d5f293181425fe3a1fe0573b48492061b8c8e
Steven Barnhurst <steven.barnhurst@convergys.com>
2017-06-06 22:59:53 -0600

    here

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component, OnInit } from '@angular/core';
 
+// move to hero/interface
 interface Hero {
   id: number;
   name: string;
 }
 
+// move to app.component
 @Component({
   selector: 'app-root',
   template: `
@@ -31,12 +33,14 @@ export class AppComponent implements OnInit {
 })
 export class AppModule { }
 
+// move to hero/service
 const HEROES: Hero[] = [
   {id: 1, name: 'Bombasto'},
   {id: 2, name: 'Tornado'},
   {id: 3, name: 'Magneta'},
 ];
 
+// move to hero/service
 function getHeroes(): Promise<Hero[]> {
   return Promise.resolve(HEROES); // TODO: get hero data from the server;
 }
